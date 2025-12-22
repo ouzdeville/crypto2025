f70fcec7347cc59c045d9ae72909baf0d0a96046
Ousmane <ouzdeville@gmail.com>
2025-12-22 14:32:46 +0000

    commit

---
+++ src/com/dcssi/cfc/crypto/com/dcssi/cfc/crypto/CryptoImpl.java
@@ -0,0 +1,135 @@
+package com.dcssi.cfc.crypto;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.awt.Point;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+public class CryptoImpl implements ICrypto {
+
+    @Override
+    public byte[] generateSeedTrullyRandom() {
+        // je veux recuperer des coordonnees de la souris pour avoir une bonne entropie
+        // en do while jusqu'a avoir ICrypto.keysize/8 bytes
+        byte[] seed = new byte[ICrypto.keysize / 8];
+        java.awt.Point precedent = new Point();
+        java.awt.Point current = new Point();
+
+        int i = 0;
+        do {
+            current = java.awt.MouseInfo.getPointerInfo().getLocation();
+            if (!current.equals(precedent)) {
+                // on a un nouveau point
+                seed[i] = (byte) (current.x);
+                i += 1;
+                precedent = current;
+            }
+        } while (i < seed.length);
+        return seed;
+    }
+
+    @Override
+    public SecretKey generateKey() {
+        try { 
+               byte[] seed = generateSeedTrullyRandom();
+               java.security.SecureRandom sr = java.security.SecureRandom.getInstance("SHA1PRNG");
+               sr.setSeed(seed);
+                javax.crypto.KeyGenerator kg = javax.crypto.KeyGenerator.getInstance(ICrypto.algo);
+                kg.init(ICrypto.keysize, sr);
+                SecretKey sk = kg.generateKey();
+                return sk;
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+   
+
+    @Override
+    public String bytesToHex(byte[] tab) {
+        // convertir un tableau de byte en chaine hexadécimale
+        StringBuilder sb = new StringBuilder();
+        for (byte b : tab) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+    @Override
+    public byte[] hextoBytes(String chaine) {
+        int len = chaine.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(chaine.charAt(i), 16) << 4)
+                    + Character.digit(chaine.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    @Override
+    public boolean cipherProcess(SecretKey k, String inputFile, String outputFile, int mode, boolean deleteAfter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cipherProcess'");
+    }
+
+    @Override
+    public boolean cipherProcessFolder(SecretKey k, String inputFolder, String outputFolder, int mode,
+            boolean deleteAfter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cipherProcessFolder'");
+    }
+
+     @Override
+    public SecretKey generatePBEKey(String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generatePBEKey'");
+    }
+
+    @Override
+    public KeyPair generateKeyPair(byte[] seed) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateKeyPair'");
+    }
+
+    @Override
+    public Key loadHexKey(String chemin, String password, int type) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadHexKey'");
+    }
+
+    @Override
+    public boolean saveHexKey(Key k, String chemin, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveHexKey'");
+    }
+
+    @Override
+    public boolean HybridEnCrypt(PublicKey k, String fileToencrypt, String encryptedFile) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'HybridEnCrypt'");
+    }
+
+    @Override
+    public boolean HybridDeCrypt(PrivateKey k, String fileToencrypt, String encreptedFile) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'HybridDeCrypt'");
+    }
+
+    @Override
+    public byte[] processData(byte[] claire, SecretKey secretKey, int mode, IvParameterSpec ivParam) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'processData'");
+    }
+
+    @Override
+    public byte[] processData(byte[] claire, Key key, int mode, IvParameterSpec ivParam) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'processData'");
+    }
+
+}
--- src/com/dcssi/cfc/crypto/com/dcssi/cfc/crypto/ICrypto.java
+++ src/com/dcssi/cfc/crypto/com/dcssi/cfc/crypto/ICrypto.java
@@ -1,4 +1,4 @@
-
+package com.dcssi.cfc.crypto;
 
 import java.security.Key;
 import java.security.KeyPair;
@@ -21,14 +21,12 @@ public interface ICrypto {
     public final String kdf = "PBKDF2WithHmacSHA256";
     public final int iteration = 1000;
     public final byte[] salt = "MO5-°HG3YEH255367gdsjhgd".getBytes();
-    //Faire un programme pour recupérer un seed avec une bonne entropie
+    // Faire un programme pour recupérer un seed avec une bonne entropie
     public static final String algoAsym = "RSA";
     public static final String algoSign = "SHA256withECDSA";
     public static final int keysizeAsym = 1024;
     public static final String transformAsym = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
 
-    
-   
     /**
      * Recupérer des coordonnees différentes de la souris de l'utilisateur pour
      * avoir 256 bits aléatoires.
@@ -36,8 +34,7 @@ public interface ICrypto {
      * @return
      */
     public byte[] generateSeedTrullyRandom();
-       
-      
+
     /**
      * la méthode generateKey permet de générer une clé à partir des paramètres
      * spécifiés.
@@ -46,37 +43,55 @@ public interface ICrypto {
      */
     public SecretKey generateKey();
 
-    
     public SecretKey generatePBEKey(String password);
 
     public String bytesToHex(byte[] tab);
 
     public byte[] hextoBytes(String chaine);
 
-    
+    /**
+     * Chiffrement ou déchiffrement d'un fichier avec une clé symétrique
+     * 
+     * @param k
+     * @param inputFile   les fichier à chiffrer ou déchiffrer
+     * @param outputFile  le fichier chiffré ou déchiffré
+     * @param mode        Cipher.ENCRYPT_MODE ou Cipher.DECRYPT_MODE pour
+     *                    chiffrement ou déchiffrement
+     * @param deleteAfter si true supprime le fichier source après traitement
+     * @return
+     */
+    public boolean cipherProcess(SecretKey k, String inputFile, String outputFile, int mode, boolean deleteAfter);
+
+    /**
+     * Chiffrement ou déchiffrement d'un dossier avec une clé symétrique
+     * 
+     * @param k
+     * @param inputFolder le dossier à chiffrer ou déchiffrer
+     * @param outputFolder le dossier chiffré ou déchiffré
+     * @param mode            Cipher.ENCRYPT_MODE ou Cipher.DECRYPT_MODE pour
+     *                        chiffrement ou déchiffrement
+     * @param deleteAfter     si true supprime le dossier source après traitement
+     * @return
+     */
+    public boolean cipherProcessFolder(SecretKey k, String inputFolder, String outputFolder, int mode,
+            boolean deleteAfter);
+
+
+
 
     
-    
-    
-
-    public boolean cipherProcess(SecretKey k, String fileToencrypt, String encryptedFile, int mode, boolean deleteAfter);
-    
-    public boolean cipherProcessFolder(SecretKey k, String FolderToencrypt, String encryptedFolder, int mode, boolean deleteAfter);
-
-    
-    public KeyPair generateKeyPair(byte [] seed);
+    public KeyPair generateKeyPair(byte[] seed);
 
     public Key loadHexKey(String chemin, String password, int type);
+
     public boolean saveHexKey(Key k, String chemin, String password);
-    
+
     public boolean HybridEnCrypt(PublicKey k, String fileToencrypt, String encryptedFile);
 
     public boolean HybridDeCrypt(PrivateKey k, String fileToencrypt, String encreptedFile);
-    
-    public byte[] processData(byte [] claire, SecretKey secretKey, int mode, IvParameterSpec ivParam);
-    public byte[] processData(byte [] claire, Key key, int mode, IvParameterSpec ivParam);
 
-   
+    public byte[] processData(byte[] claire, SecretKey secretKey, int mode, IvParameterSpec ivParam);
 
-    
+    public byte[] processData(byte[] claire, Key key, int mode, IvParameterSpec ivParam);
+
 }
